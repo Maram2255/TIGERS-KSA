83f0d606368986194c71f96439f5f726762ff2c0
Maram TIGERS.. Alharbi <maramaljabri22@gmail.com>
2025-08-22 05:02:17 +0300

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        turquoise: "#30CFCF",
+        white: "#FFFFFF"
+      }
+    }
+  },
+  plugins: []
+};
