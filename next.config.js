ec88ccec46d0232b4e5157c892b52130f5b4b186
Maram TIGERS.. Alharbi <maramaljabri22@gmail.com>
2025-08-23 04:44:40 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.(glb|gltf)$/,
-      use: [{ loader: "url-loader" }]
-    });
-    return config;
-  }
-};
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
